b3c692b3a44c2f8460f45f19a3af6b3f9c14e028
Elizaveta139 <elizavetarevina139@gmail.com>
2024-06-10 11:42:51 +0300

    Updates

---
+++ assets/FavoritesPage-BC72zhw1.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-BbfDYZvk.js";import{H as r,a as t}from"./index.esm-CizdmJ7y.js";function a(){return e.jsx(e.Fragment,{children:e.jsx(r,{children:e.jsx(t,{children:e.jsx("title",{children:"FavoritesPage"})})})})}export{a as default};
+//# sourceMappingURL=FavoritesPage-BC72zhw1.js.map
