ad193badf3b7b82edd2aed25a0d9e565b41c6489
Elizaveta139 <elizavetarevina139@gmail.com>
2024-06-10 12:33:26 +0300

    Updates

--- assets/FavoritesPage-BmMH0mYR.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-C8yEGXFZ.js";import{H as r,a as t}from"./index.esm-H7s1CIu2.js";function a(){return e.jsx(e.Fragment,{children:e.jsx(r,{children:e.jsx(t,{children:e.jsx("title",{children:"FavoritesPage"})})})})}export{a as default};
-//# sourceMappingURL=FavoritesPage-BmMH0mYR.js.map
---
+++ assets/FavoritesPage-DrorSb7B.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-m7ZXlWO4.js";import{H as r,a as t}from"./index.esm-BuqRID-R.js";function a(){return e.jsx(e.Fragment,{children:e.jsx(r,{children:e.jsx(t,{children:e.jsx("title",{children:"FavoritesPage"})})})})}export{a as default};
+//# sourceMappingURL=FavoritesPage-DrorSb7B.js.map
