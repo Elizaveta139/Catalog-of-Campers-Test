31be682f0747718b4eecbf7fa6cb0b8af6b0861a
Elizaveta139 <elizavetarevina139@gmail.com>
2024-06-10 11:27:18 +0300

    Updates

---
+++ assets/FavoritesPage-DQtsS2_Q.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-WNQm3mHi.js";import{H as r,a as t}from"./index.esm-CH8qpfO1.js";function a(){return e.jsx(e.Fragment,{children:e.jsx(r,{children:e.jsx(t,{children:e.jsx("title",{children:"FavoritesPage"})})})})}export{a as default};
+//# sourceMappingURL=FavoritesPage-DQtsS2_Q.js.map
