6686c9c9149aa48928d5524e8c3d4bfcc458be4b
Elizaveta139 <elizavetarevina139@gmail.com>
2024-06-10 12:35:37 +0300

    upd

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -10,7 +10,7 @@ import './index.module.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Catalog-of-Campers-Test/">
         <App />
       </BrowserRouter>
     </Provider>
