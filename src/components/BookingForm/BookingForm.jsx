8a9b7e90d8c8d8d65c3f7f95e0698d616fedb677
Elizaveta139 <elizavetarevina139@gmail.com>
2024-06-20 11:41:54 +0300

    Update BookingForm.jsx

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -5,6 +5,9 @@ export default function BookingForm() {
     <div className={css.bookingFormSection}>
       <h3 className={css.title}>Book your campervan now</h3>
       <p className={css.subTitle}>Stay connected! We are always ready to help you.</p>
+      <form action="">
+        <input type="text" />
+      </form>
     </div>
   );
 }
